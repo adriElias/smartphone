7348acc8dd4676f50b6158b36f87d85e613edbdc
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-07 14:17:13 +0100

    feat: create phone class and interfaces camera and clock

--- src/main/java/org/smartphone/Phone.java
+++ src/main/java/org/smartphone/Phone.java
@@ -1,4 +1,24 @@
 package org.smartphone;
 
-public class Phone {
+public class Phone implements Camera, Clock {
+    private String brand;
+    private String model;
+
+    public Phone(String brand, String model) {
+        this.brand = brand;
+        this.model = model;
+    }
+    public void calls(int number){
+        System.out.println("Calling: " + number);
+    }
+    @Override
+    public void takePhoto(){
+        System.out.println("Taking a photo");
+    }
+
+    @Override
+    public void alarm(){
+        System.out.println("Ringing alarm");
+    }
+
 }
