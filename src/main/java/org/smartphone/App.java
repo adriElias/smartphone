5d22c431da3565970babbdabc4a468a54d4c2258
adriElias <34091624+adriElias@users.noreply.github.com>
2026-02-09 13:17:20 +0100

    feat: Create Smartphone class extending phone and create smartphone demo for call, photo, and alarm

--- src/main/java/org/smartphone/App.java
+++ src/main/java/org/smartphone/App.java
@@ -1,13 +1,21 @@
 package org.smartphone;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("---- Phone ----");
+        Smartphone smartphone1 = new Smartphone("iPhone", "13 PRO MAX");
+        smartphone1.call("72272232");
+        smartphone1.takePhoto();
+        smartphone1.alarm();
+
+        System.out.println("---- Polymorphism ----");
+        Phone phone1 = smartphone1;
+        phone1.call("999999999");
+
+        Camera camera1 = smartphone1;
+        camera1.takePhoto();
+
+        Clock clock1 = smartphone1;
+        clock1.alarm();
     }
 }
--- src/main/java/org/smartphone/Phone.java
+++ src/main/java/org/smartphone/Phone.java
@@ -1,23 +1,25 @@
 package org.smartphone;
 
 public class Phone implements Camera, Clock {
-    private String brand;
-    private String model;
+    private final String brand;
+    private final String model;
 
     public Phone(String brand, String model) {
         this.brand = brand;
         this.model = model;
     }
-    public void calls(int number){
+
+    public void call(String number) {
         System.out.println("Calling: " + number);
     }
+
     @Override
-    public void takePhoto(){
+    public void takePhoto() {
         System.out.println("Taking a photo");
     }
 
     @Override
-    public void alarm(){
+    public void alarm() {
         System.out.println("Ringing alarm");
     }
 
